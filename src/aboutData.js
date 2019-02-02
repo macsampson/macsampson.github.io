1cda691f1fb6fb5404aaaaf179ceb4ac3d977640
Mac Sampson <mackenzie.sampson@outlook.com>
2019-02-02 15:49:55 -0800

    Updates

--- src/aboutData.js
+++
@@ -1,4 +0,0 @@
-let aboutData = {
-  aboutMe: "Im a guy",
-  contact: "mac sampson"
-};
--- src/resumeData.js
+++
@@ -1,9 +0,0 @@
-let resumeData = {
-  imagebaseurl: "https://macsampson.github.io/",
-  name: "Mac Sampson",
-  role: "Software Engineer",
-  education: [],
-  work: []
-};
-
-export default resumeData;
