3ec68390d3dac9235f513421867f3c13821ff3fb
Mac Sampson <mac.sampson@protonmail.com>
2020-05-15 14:45:07 -0700

    Add company logos, artwork, project to resume data file

--- src/resumeData.js
+++ src/resumeData.js
@@ -4,6 +4,7 @@ const resumeData = {
       id: 0,
       position: `Technical Artist`,
       company: `Electronic Arts`,
+      logo: `images/ea.png`,
       dates: `Sept 2019 - Present`,
       technology: `C#, Python`,
       desc1: `• Developed solutions using Python and C# to parse, aggregate, and restructure repository assets to adhere to new conventions for upcoming game title`,
@@ -14,6 +15,7 @@ const resumeData = {
       id: 1,
       position: `Software Compliance Engineer Intern`,
       company: `SAP`,
+      logo: `images/sap.png`,
       dates: `May - Dec 2017`,
       technology: `Python, Selenium`,
       desc1: `• Worked with a team to ensure that SAP operated within legal compliance when software engineers wished to incorporate
@@ -28,6 +30,7 @@ const resumeData = {
       id: 2,
       position: `Software Engineer Intern`,
       company: `MDA`,
+      logo: `images/mda.png`,
       dates: `Jan - Sept 2016`,
       technology: `Javascript, HTML/CSS, C#`,
       desc1: `• Worked closely with the marketing team to meet their
@@ -43,6 +46,7 @@ const resumeData = {
       id: 3,
       position: `Software Engineering Teaching Assistant`,
       company: `UBC`,
+      logo: `images/ubc.png`,
       dates: `Sept - Dec 2015`,
       technology: `Python, Django, Rails`,
       desc1: `• Handpicked by course professor to assist in managing
@@ -55,6 +59,7 @@ const resumeData = {
   ],
   education: [
     {
+      logo: `images/ubc.png`,
       school: `University of British Columbia`,
       program: `Bachelor of Computer Science`,
       dates: `Sept 2014 - May 2018`
@@ -64,18 +69,89 @@ const resumeData = {
     //   program: `Bachelor of Business Administration`,
     //   dates: `Sept 2008 - May 2014`
     // }
-  ]
+  ],
+  languages: [`TypeScript`, `C#`, `Python`, `Java`, `SQL`],
+  tech: [
+    `React`,
+    `Docker`,
+    `Node.js`,
+    `AWS`,
+    `MongoDB`,
+    `Selenium`,
+    `Express.js`,
+    `HTML5/CSS3`
+  ],
+  projects: [
+    {
+      name: `VanCity Parking`,
+      type: `MERN App`,
+      modal: `modal-01`,
+      img: `images/portfolio/parkr_tile.jpg`,
+      alt: `vancity`,
+      desc: `A MERN app that plots all public parking meters in the Metro
+                Vancouver area, along with their rates and hours of operation.
+                There will also be an additional option to overlay a heatmap with
+                all auto-related crime, so you can feel more at ease parking in
+                certain neighbourhoods.`,
+      link: `https://github.com/macsampson/VanCity-Parking`,
+      modalImg: `images/portfolio/modals/parkr.jpg`,
+      isLive: true,
+      appLink: `http://www.vancityparking.com/`
+    },
+    {
+      name: `Game Deals`,
+      type: `Django App`,
+      modal: `modal-02`,
+      img: `images/portfolio/gamedeals_tile.jpg`,
+      alt: `game deals`,
+      desc: `A Django app that uses the Reddit API to pull game sale
+              information from r/gamedeals and associate the games with their
+              respective artwork obtained from the IGDB API.`,
+      link: `https://github.com/macsampson/GameDeals-Aggregator`,
+      isLive: false,
+      modalImg: `images/portfolio/modals/gamedeals.jpg`
+    }
+  ],
+  artwork: {
+    display: false,
+    art: [
+      {
+        name: `Panther`,
+        type: `Illustration`,
+        modal: `modal-03`,
+        img: `images/portfolio/panther_tile.jpg`,
+        alt: `panther`,
+        modalName: `Wakanda Forever`,
+        desc: `Recreated a piece in Photoshop originally done by one of my
+                favourite illustrators: @marcusthevisual`,
+        link: `https://www.artstation.com/artwork/aREzGz`,
+        modalImg: `images/portfolio/modals/panther_large.jpg`
+      },
+      {
+        name: `Tracer`,
+        type: `Illustration`,
+        modal: `modal-04`,
+        img: `images/portfolio/tracer_tile.jpg`,
+        alt: `tracer`,
+        modalName: `Cheers, Love`,
+        desc: `My first attempt at digital art. I painted this in Photoshop just
+                before the release of Overwatch.`,
+        link: `https://www.artstation.com/artwork/aaZXq`,
+        modalImg: `images/portfolio/modals/tracer_large.jpg`
+      },
+      {
+        name: `Emma`,
+        type: `Illustration`,
+        modal: `modal-05`,
+        img: `images/portfolio/emma_tile.jpg`,
+        alt: `emma`,
+        modalName: `Amortentia`,
+        desc: `I watched too much Harry Potter during my impressionable years.`,
+        link: `https://www.artstation.com/artwork/qAo1eL`,
+        modalImg: `images/portfolio/modals/emma_large.jpg`
+      }
+    ]
+  }
 };
-export const languages = [`TypeScript`, `C#`, `Python`, `Java`, `SQL`];
-export const tech = [
-  `React`,
-  `Docker`,
-  `Node.js`,
-  `AWS`,
-  `MongoDB`,
-  `Selenium`,
-  `Express.js`,
-  `HTML5/CSS3`
-];
 
 export default resumeData;
