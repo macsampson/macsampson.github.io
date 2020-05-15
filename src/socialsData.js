50d913c5b59120ec047c59117097d4efc5fa0595
Mac Sampson <mac.sampson@protonmail.com>
2020-05-15 13:12:35 -0700

    Create data files for social links and all bio data

---
+++ src/socialsData.js
@@ -0,0 +1,29 @@
+const socialsData = [
+  {
+    id: 0,
+    link: "https://github.com/macsampson",
+    icon: "fab fa-github"
+  },
+  {
+    id: 1,
+    link: "https://linkedin.com/in/macsampson",
+    icon: "fab fa-linkedin"
+  },
+  {
+    id: 2,
+    link: "https://medium.com/@mackenzie.sampson",
+    icon: "fab fa-medium-m"
+  },
+  {
+    id: 3,
+    link: "https://twitter.com/themacsampson",
+    icon: "fab fa-twitter"
+  },
+  {
+    id: 4,
+    link: "mailto:mackenzie.sampson@outlook.com",
+    icon: "fa fa-envelope"
+  }
+];
+
+export default socialsData;
