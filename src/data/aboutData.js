2d27f9760ebe73bd5b06c04c62a8bbb815b12cb0
Mac <mac.sampson@pm.me>
2021-06-09 12:59:41 -0700

    update about me

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -5,8 +5,7 @@ const aboutData = {
     scrolling to learn more about me.`
   },
   aboutMe: {
-    p1: `Hey, I'm Mac Sampson. I am a software engineer with experience in full-stack web development, tools engineering, and automation software. 
-    I am passionate about working on software that can improve people's quality of life. In my spare time, you can find me tinkering with React, building tools to try to connect the people around us. 
+    p1: `Hey, I'm Mac. I am a software engineer with experience in full-stack web development, tools engineering, and automation.
     `,
     p2: `Outside of engineering, I enjoy spending time setting up new projects on my Raspberri Pi, finding new and useful ways to utilize my 3D printer, and gathering friends together for late night gaming sessions.`
   },
